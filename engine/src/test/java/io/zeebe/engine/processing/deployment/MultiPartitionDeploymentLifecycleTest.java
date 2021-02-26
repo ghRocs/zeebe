d3f737cc7dfb9e8fdf8adf00faec13ce94d231ac
Christopher Zell <zelldon91@googlemail.com>
2021-02-26 15:14:10 +0100

    chore(engine): migrate deployment processor to event sourcing

---
+++ engine/src/test/java/io/zeebe/engine/processing/deployment/MultiPartitionDeploymentLifecycleTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.0. You may not use this file
+ * except in compliance with the Zeebe Community License 1.0.
+ */
+package io.zeebe.engine.processing.deployment;
+
+import static io.zeebe.protocol.Protocol.DEPLOYMENT_PARTITION;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.zeebe.engine.util.EngineRule;
+import io.zeebe.model.bpmn.Bpmn;
+import io.zeebe.model.bpmn.BpmnModelInstance;
+import io.zeebe.protocol.record.Record;
+import io.zeebe.protocol.record.RecordType;
+import io.zeebe.protocol.record.intent.DeploymentDistributionIntent;
+import io.zeebe.protocol.record.intent.DeploymentIntent;
+import io.zeebe.protocol.record.intent.WorkflowIntent;
+import io.zeebe.test.util.record.RecordingExporter;
+import io.zeebe.test.util.record.RecordingExporterTestWatcher;
+import java.util.stream.Collectors;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MultiPartitionDeploymentLifecycleTest {
+
+  public static final String PROCESS_ID = "process";
+  public static final int PARTITION_ID = DEPLOYMENT_PARTITION;
+  public static final int PARTITION_COUNT = 3;
+
+  @ClassRule public static final EngineRule ENGINE = EngineRule.multiplePartition(PARTITION_COUNT);
+
+  @Rule
+  public final RecordingExporterTestWatcher recordingExporterTestWatcher =
+      new RecordingExporterTestWatcher();
+
+  @Test
+  public void shouldTestLifecycle() {
+    // given - reprocess
+    final BpmnModelInstance modelInstance =
+        Bpmn.createExecutableProcess("shouldReDistributeAfterRecovery")
+            .startEvent()
+            .endEvent()
+            .done();
+
+    // when
+    ENGINE.deployment().withXmlResource("process.bpmn", modelInstance).deploy();
+
+    // then
+    final var deploymentPartitionRecords =
+        RecordingExporter.records().withPartitionId(1).limit(14).collect(Collectors.toList());
+
+    assertThat(deploymentPartitionRecords)
+        .extracting(Record::getIntent)
+        .containsExactly(
+            DeploymentIntent.CREATE,
+            WorkflowIntent.CREATED,
+            DeploymentIntent.CREATED,
+            DeploymentDistributionIntent.DISTRIBUTING,
+            DeploymentDistributionIntent.DISTRIBUTING,
+            DeploymentDistributionIntent.COMPLETE,
+            DeploymentDistributionIntent.COMPLETE,
+            DeploymentDistributionIntent.COMPLETED,
+            DeploymentDistributionIntent.COMPLETED,
+            DeploymentIntent.FULLY_DISTRIBUTED);
+
+    assertThat(deploymentPartitionRecords)
+        .extracting(Record::getRecordType)
+        .containsExactly(
+            RecordType.COMMAND,
+            RecordType.EVENT,
+            RecordType.EVENT,
+            RecordType.EVENT,
+            RecordType.EVENT,
+            RecordType.COMMAND,
+            RecordType.COMMAND,
+            RecordType.EVENT,
+            RecordType.EVENT,
+            RecordType.EVENT);
+
+    assertThat(RecordingExporter.records().withPartitionId(2).limit(2).collect(Collectors.toList()))
+        .extracting(Record::getIntent)
+        .containsExactly(DeploymentIntent.DISTRIBUTE, DeploymentIntent.DISTRIBUTED);
+
+    assertThat(RecordingExporter.records().withPartitionId(3).limit(2).collect(Collectors.toList()))
+        .extracting(Record::getIntent)
+        .containsExactly(DeploymentIntent.DISTRIBUTE, DeploymentIntent.DISTRIBUTED);
+  }
+}
